786d0c31313fa984c60bcfc224d1d6845309f3f6
Jianyan Li <simonli1215@outlook.com>
2018-11-25 21:43:21 -0500

    string exercise

---
+++ data_types/string.js
@@ -0,0 +1,16 @@
+//embeded expression and multiple lines
+console.log(`1+1 = ${1 + 1}`);
+let me = ` simon
+li
+jianyan `;
+
+console.log(me);
+//newline character
+console.log("I am \n your \n father");
+
+//unicode
+
+console.log("\u{1F60D}");
+
+//escape character
+console.log('I\'m')
