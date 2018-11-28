3d2a30d375ba9a98e1e2dbb286b7480abd18954c
Jianyan Li <simonli1215@outlook.com>
2018-11-27 21:03:20 -0500

    update change of title function

--- document/image.js
+++ document/image.js
@@ -1,5 +1,22 @@
+"use strict";
 let placeHolder = document.getElementById("placeholder");
 placeHolder.style.width = "50%";
 function showPic(whichpic) {
-	
+	let source = whichpic.getAttribute("href");
+	let placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src", source);
+	let text = whichpic.getAttribute("title");
+	let description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
 }
+
+function countBodyChildren() {
+	let body = document.getElementsByTagName("body")[0];
+	for (let children = 0; children < 5; ++children ) {
+		console.log(body.childNodes[children]);
+	}
+	console.log(body.childNodes.length);
+	console.log(body.nodeType);
+	//element nodes = 1; attribute node = 2; text node = 3
+}
+window.onload = countBodyChildren;
