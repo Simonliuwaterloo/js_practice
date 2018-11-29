6a3504f100e3409adf8995141f2a4a5b03879023
Jianyan Li <simonli1215@outlook.com>
2018-11-29 11:06:35 -0500

    update environment chech
    1. getElementById available
    2. getElementsByClass available
    3. getElementsBtTag avalible

--- document/image.js
+++ document/image.js
@@ -1,13 +1,21 @@
 "use strict";
 let placeHolder = document.getElementById("placeholder");
 placeHolder.style.width = "50%";
+
 function showPic(whichpic) {
+	if (!document.getElementById("placeholder")) return false;
 	let source = whichpic.getAttribute("href");
 	let placeholder = document.getElementById("placeholder");
+	if (placeholder.nodeName != "IMG") return false;
 	placeholder.setAttribute("src", source);
-	let text = whichpic.getAttribute("title");
-	let description = document.getElementById("description");
-	description.firstChild.nodeValue = text;
+	if (document.getElementById("description")) {
+		let text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+		let description = document.getElementById("description");
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text;
+		}
+	}
+	return true;
 }
 
 function countBodyChildren() {
@@ -20,12 +28,29 @@ function countBodyChildren() {
 	//element nodes = 1; attribute node = 2; text node = 3
 }
 
+function popUp() {
+	console.log("rs");
+	if (!document.getElementsByClassName) return false;
+	let links = document.getElementsByClassName("popup")
+		for (let i = 0; i < links.length; ++i) {
+			links[i].onclick = function() {
+			window.open(this.href);
+			return false;
+			}
+		}
+}
+
 function prepareGallery() {
-	if (!document.getElementsByTagName && document.getElementById("imagegallery")) return false;
+	if (!document.getElementsByTagName || !document.getElementById("imagegallery")) return false;
 	let gallery = document.getElementById("imagegallery");
 	let links = gallery.getElementsByTagName("a");
-}	for (let i = 0; i < links.length; ++i) {
-	links[i].onclick = function() {
-		showPic(this); return false;
+	for (let i = 0; i < links.length; ++i) {
+		links[i].onclick = function() {
+			return !showPic(this);
 		}
 	}
+}
+window.onload = function() {
+	popUp();
+	prepareGallery();
+}
