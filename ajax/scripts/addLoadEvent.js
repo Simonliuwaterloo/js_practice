1e29b5c99952af048d5290f4bbbe56b1121c396d
Jianyan Li <simonli1215@outlook.com>
2019-01-02 11:38:47 -0500

    put function add load event in addloadevent.js

---
+++ ajax/scripts/addLoadEvent.js
@@ -0,0 +1,12 @@
+function addLoadEvent(func) {
+	let oldonload = window.onload;
+	if (typeof window.onload != "function") {
+		window.onload = func;
+	}
+	else {
+		window.onload = function () {
+			oldonload();
+			func();
+		}
+	}
+}
--- ajax/scripts/getNewContent.js
+++ ajax/scripts/getNewContent.js
@@ -17,15 +17,4 @@ function getNewContent() {
 	}
 }
 addLoadEvent(getNewContent);
-function addLoadEvent(func) {
-	let oldonload = window.onload;
-	if (typeof window.onload != "function") {
-		window.onload = func;
-	}
-	else {
-		window.onload = function () {
-			oldonload();
-			func();
-		}
-	}
-}
+
